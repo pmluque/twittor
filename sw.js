12481694be7084ffcef8eb92bc3d957ef345c15c
Pedro <pmluque@gmail.com>
2020-12-21 18:46:32 +0100

    Modificaciones para IOS

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@ importScripts('js/sw-utils.js');
 
 
 // PWA.7.2 - constantes de cache y limites de las mismas
-const STATIC_CACHE = 'static-v1';
+const STATIC_CACHE = 'static-v2';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
